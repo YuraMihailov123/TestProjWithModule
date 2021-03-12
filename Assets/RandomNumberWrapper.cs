48bfa0cff45c07c087a600d7e149f746d14002c7
YuraMihailov <recon.97@mail.ru>
2021-03-12 18:05:53 +0300

    Initial commit

---
+++ Assets/RandomNumberWrapper.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RandomNumber;
+
+public static class RandomNumberWrapper
+{
+
+    public static void Init()
+    {
+        if (!PlayerPrefs.HasKey("init"))
+        {
+            PlayerPrefs.SetInt("init", MyClass.GetRandomNumber());
+        }
+        DoActions(PlayerPrefs.GetInt("init"));
+    }
+
+    static void DoActions(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                Debug.Log(Application.productName);
+                break;
+            case 1:
+                Application.OpenURL("https://yandex.ru");
+                break;
+        }
+    }
+}
